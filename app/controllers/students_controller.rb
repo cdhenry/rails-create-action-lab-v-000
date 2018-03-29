fbbc36339fb9330510f22be26104680738e0180b
Chris Henry <cdhenry@gmail.com>
2018-03-29 00:14:43 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Post.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to post_path(@student)
   end
 
 end
